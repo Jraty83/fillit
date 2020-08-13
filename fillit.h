40f66b0ee1cc79fcf89d08a06bb1745b22a36c35
Janne Raty <jraty@c1r6p13.hive.fi>
2020-08-13 14:50:06 +0300

    BUF_SIZE set to 21

--- fillit.h
+++ fillit.h
@@ -6,12 +6,16 @@
 /*   By: jraty <jraty@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 12:30:12 by jraty             #+#    #+#             */
-/*   Updated: 2020/08/13 11:37:52 by jraty            ###   ########.fr       */
+/*   Updated: 2020/08/13 12:16:42 by jraty            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
+
+#include "/Users/jraty/libft/libft.h"
+#include "/Users/jraty/libft/get_next_line.h"
+#include <fcntl.h>
 
 # define BUF_SIZE 21
 
